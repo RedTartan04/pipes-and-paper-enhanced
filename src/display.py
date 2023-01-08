a5c83ad01f9d8e766282d68f5fca9217353a2007
RedTartan04 <99086095+RedTartan04@users.noreply.github.com>
2023-01-08 10:06:16 +0100

    get framebuffer content and put into PIL image, save as ping for test

--- src/display.py
+++ src/display.py
@@ -1,6 +1,7 @@
 """Capture screenshot of the reMarkable."""
 
 import math
+from PIL import Image
 from src.connection import run_ssh_cmd
 
 
@@ -14,14 +15,17 @@ class Display:
     fb_size = screenwidth * screenheight
     fb_pagesize = 4096
 
+    capture_fb_cmd = ""
+
     def __init__(self, ssh_hostname: str) -> None:
         self.ssh_hostname = ssh_hostname
+
 
     def get_fb_location(self):
         """Determine framebuffer memory location and create capture command.
            Location changes after restart."""
 
-        # get rM software's process id
+        # Get rM software's process id.
         pid = run_ssh_cmd(self.ssh_hostname, "pidof", "xochitl")
         if (pid):
             pid = pid.strip()
@@ -31,7 +35,7 @@ class Display:
         #'4210'
         print(f"{pid=}")
 
-        # in-memory framebuffer location is just after the noise from /dev/fb0.
+        # In-memory framebuffer location is just after the noise from /dev/fb0.
 
         #grep -C1 '/dev/fb0' /proc/4210/maps | tail -n1 | sed 's/-.*$//'
         cmd_params = f"-C1 '/dev/fb0' /proc/{pid}/maps | tail -n1 | sed 's/-.*$//'"
@@ -63,4 +67,43 @@ class Display:
         print(f"{self.capture_fb_cmd=}")
 
 
+    def get_fb(self):
+        """Get current framebuffer content."""
+
+        # First get framebuffer location, if not already done.
+        if (self.capture_fb_cmd == ""):
+            self.get_fb_location()
+
+        # Capture framebuffer content.
+        raw_fb = run_ssh_cmd(self.ssh_hostname, "dd", self.capture_fb_cmd, raw=True)
+        if (raw_fb):
+            # Because this captured excess data (it was aligned to the page size) 
+            # we need to trim some off.
+            raw_fb = raw_fb[self.fb_offset:][:self.fb_size]        
+        else:
+            print(('* problem getting framebuffer'))
+            return
  
+        return raw_fb
+
+
+    def get_image(self, orientation='landscape'):
+        """Returns PNG data."""
+
+        raw_fb = self.get_fb()
+
+        print(f"raw_fb len={len(raw_fb)}")
+
+        # rM pixel format: 8 bit grayscale
+        # = PLI mode 'L'
+        # default decoder = 'raw'
+        image= Image.frombytes(mode = 'L', size = (self.screenwidth, self.screenheight), data = raw_fb)
+        print("PIL ", image)
+
+        ## TODO rotate for portrait
+
+        ## TODO return PNG for further use
+        image.save("rM_snapshot.png")
+
+
+
